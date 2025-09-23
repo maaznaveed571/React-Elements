8b4ba9939c00876052f53b873f0df979a0f75c66
Muhammad Maaz Naveed <maaznaveed571@gmail.com>
2025-09-24 02:40:48 +0500

    transcript added

--- src/components/player/AnumAi-Player.tsx
+++ src/components/player/AnumAi-Player.tsx
@@ -3,80 +3,80 @@ import { AnamEvent, createClient, type AnamClient } from "@anam-ai/js-sdk";
 import { Loader } from "lucide-react";
 
 const AnumAI = ({ sessionToken }: { sessionToken: string }) => {
-  const anamClientRef = useRef<AnamClient | null>(null);
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
+    const anamClientRef = useRef<AnamClient | null>(null);
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [transcript, setTranscript] = useState<string>("");
 
-  const stopVideoStreaming = async () => {
-    if (anamClientRef.current) {
-      try {
-        await anamClientRef.current.stopStreaming();
-        console.log("Stopped stream");
-      } catch (err) {
-        console.error("Error stopping stream:", err);
-      }
+    const stopVideoStreaming = async () => {
+        if (anamClientRef.current) {
+            try {
+                await anamClientRef.current.stopStreaming();
+                console.log("Stopped stream");
+            } catch (err) {
+                console.error("Error stopping stream:", err);
+            }
 
-      anamClientRef.current = null;
-      if (videoRef?.current) {
-        videoRef.current.srcObject = null;
-        videoRef.current.load();
-      }
-    }
-  };
-  useEffect(() => {
-    const setup = async () => {
-      stopVideoStreaming();
-
-      const client = createClient(sessionToken, {
-        disableInputAudio: true,
-      });
-      anamClientRef.current = client;
-
-      try {
-        await client.streamToVideoElement(`video-element-${sessionToken}`);
-
-        client.addListener(AnamEvent.SESSION_READY, () => {
-          setLoading(false);
-        });
-      } catch (err) {
-        console.error("Error starting stream:", err);
-      }
+            anamClientRef.current = null;
+            if (videoRef?.current) {
+                videoRef.current.srcObject = null;
+                videoRef.current.load();
+            }
+        }
     };
+    useEffect(() => {
+        const setup = async () => {
+            stopVideoStreaming();
 
-    setup();
+            const client = createClient(sessionToken, {
+                disableInputAudio: true,
+            });
+            anamClientRef.current = client;
 
-    return () => {
-      // Cleanup on unmount or sessionToken change
-      if (anamClientRef?.current) {
-        anamClientRef?.current.stopStreaming();
-      }
-    };
-  }, [sessionToken]);
+            try {
+                await client.streamToVideoElement(`video-element-${sessionToken}`);
 
-  return (
-    <div className="relative flex-1">
-      {loading && (
-        <div className="bg-gray-500 min-w-full h-full flex justify-center items-center absolute top-0 left-0 right-0">
-          <Loader className="animate-spin text-[2rem]" />
+                client.addListener(AnamEvent.SESSION_READY, () => {
+                    setLoading(false);
+                });
+                client.addListener(AnamEvent.MESSAGE_STREAM_EVENT_RECEIVED, (event) => {
+                    setTranscript((prev) => `${prev ? prev : ''} ${event.content}`);
+                });
+            } catch (err) {
+                console.error("Error starting stream:", err);
+            }
+        };
+
+        setup();
+
+        return () => {
+            // Cleanup on unmount or sessionToken change
+            if (anamClientRef?.current) {
+                anamClientRef?.current.stopStreaming();
+            }
+        };
+    }, [sessionToken]);
+
+    return (
+        <div className="relative flex-1">
+            {loading && (
+                <div className="bg-gray-500 min-w-full h-full flex justify-center items-center absolute top-0 left-0 right-0">
+                    <Loader className="animate-spin text-[2rem]" />
+                </div>
+            )}
+            <video
+                ref={videoRef}
+                id={`video-element-${sessionToken}`}
+                autoPlay
+                playsInline
+                onCanPlayThrough={() => setLoading(false)}
+            ></video>
+
+            {transcript !== "" && (
+                <p>{transcript}</p>
+            )}
         </div>
-      )}
-      <video
-        ref={videoRef}
-        id={`video-element-${sessionToken}`}
-        autoPlay
-        playsInline
-        onCanPlayThrough={() => setLoading(false)}
-      ></video>
-      <button
-        type="button"
-        onClick={() => {
-          anamClientRef.current?.stopStreaming();
-        }}
-      >
-        stop Play
-      </button>
-    </div>
-  );
+    );
 };
 
 export default AnumAI;
